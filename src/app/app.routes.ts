b5e9f7d0c3678f82e346de7a4da164f563cc64e6
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-12-11 17:09:58 +0700

    update: ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: '', component: AppComponent, data: { seo: { title: 'Home Page - My App', description: 'Welcome to the home page.' } } },
+];
